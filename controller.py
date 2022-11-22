370e9f5ecbd4e56267f70fa532cc11f3bcbe3113
tboonma <tboonma@hotmail.com>
2022-11-23 00:43:18 +0700

    implemented some logic for some route

--- controller.py
+++ controller.py
@@ -15,3 +15,37 @@ pool = PooledDB(creator=pymysql,
                 maxconnections=1,
                 blocking=True)
 
+def get_busstops():
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT bus_stop_id, bus_stop_name, lat, lon
+            FROM bus_stop
+        """)
+        result = [models.Busstop(*row) for row in cs.fetchall()]
+        return result
+
+def get_busstop(stop_id):
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT bus_stop_id, bus_stop_name, lat, lon
+            FROM bus_stop
+            WHERE bus_stop_id=%s
+        """, [stop_id])
+        result = cs.fetchone()
+        if result:
+            return models.Busstop(*result)
+
+def get_busstop_weather(stop_id):
+    return "Doing something"
+
+def get_routes():
+    return "Doing something"
+
+def get_takable_bus(stop_id_origin, stop_id_dest):
+    return "Doing something"
+
+def get_bus_route(bus_id):
+    return "Doing something"
+
+def get_buses():
+    return "Doing something"
