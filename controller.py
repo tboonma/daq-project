63cdd4809e971d977c157541a2822877c7f20280
tboonma <tboonma@hotmail.com>
2022-11-23 19:58:54 +0700

    Reconfig module and add env to prod

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ from dbutils.pooled_db import PooledDB
 from config import OPENAPI_STUB_DIR, DB_HOST, DB_USER, DB_PASSWD, DB_NAME
 
 sys.path.append(OPENAPI_STUB_DIR)
-from swagger_server import models
+from openapi_server import models
 
 pool = PooledDB(creator=pymysql,
                 host=DB_HOST,
