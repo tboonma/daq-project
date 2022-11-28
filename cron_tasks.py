b9cc18e66770844adb1dc1c32588b574b2fecac0
tboonma <tboonma@hotmail.com>
2022-11-28 15:25:20 +0700

    add cron job

--- cron_tasks.py
+++ cron_tasks.py
@@ -114,6 +114,63 @@ def generate_population():
             print(f"[Population] {result.name} population inserted...")
 
 
+def integrate_data():
+    print("[Integrate] Getting IQAir weather information...")
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT bus_stop_id, AVG(temperature) AS temperature, AVG(pm25_value) AS pm25, AVG(humidity) AS humidity, AVG(wind_speed) AS wind_speed
+            FROM iqair
+            WHERE `ts` > NOW() - INTERVAL 2 HOUR
+            GROUP BY bus_stop_id
+        """)
+        insert_query = """
+            INSERT into weather (bus_stop_id, ts, sensor, source, value)
+            VALUES
+        """
+        for data in cs.fetchall():
+            busstop_id, temperature, pm25, humidity, wind_speed = data
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'temperature', 'iqair', {temperature}),"
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'pm25', 'iqair', {pm25}),"
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'humidity', 'iqair', {humidity}),"
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'windspeed', 'iqair', {wind_speed}),"
+        query = insert_query[:-1] + ";"
+        cs.execute(query)
+        conn.commit()
+    print("[Integrate] Getting AQICN weather information...")
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT bus_stop_id, AVG(value) AS pm25
+            FROM aqi
+            WHERE `ts` > NOW() - INTERVAL 2 HOUR
+            GROUP BY bus_stop_id
+        """)
+        insert_query = """
+            INSERT into weather (bus_stop_id, ts, sensor, source, value)
+            VALUES
+        """
+        for data in cs.fetchall():
+            busstop_id, pm25 = data
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'pm25', 'aqi', {pm25}),"
+        query = insert_query[:-1] + ";"
+        cs.execute(query)
+        conn.commit()
+    print("[Integrate] Getting Open Meteo weather information...")
+    with pool.connection() as conn, conn.cursor() as cs:
+        cs.execute("""
+            SELECT bus_stop_id, AVG(temperature) AS temperature, AVG(windspeed) AS windspeed
+            FROM open_meteo
+            WHERE `ts` > NOW() - INTERVAL 2 HOUR
+            GROUP BY bus_stop_id
+        """)
+        for data in cs.fetchall():
+            busstop_id, temperature, wind_speed = data
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'temperature', 'open_meteo', {temperature}),"
+            insert_query += f" ({busstop_id}, '{datetime.now(th_timezone)}', 'windspeed', 'open_meteo', {wind_speed}),"
+        query = insert_query[:-1] + ";"
+        cs.execute(query)
+        conn.commit()
+
+
 if __name__ == '__main__':
     if sys.argv[1] != None:
         globals()[sys.argv[1]]()
